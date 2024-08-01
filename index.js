7fd06e773cc0125d6d4858d9cdf9b7c9936fa08d
Juantorreser <128902947+Juantorreser@users.noreply.github.com>
2024-08-01 10:40:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -80,10 +80,7 @@ app.use((req, res, next) => {
 
 //mongoose connection
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: true,
-    sslValidate: true
+    serverSelectionTimeoutMS: 20000, // 20 seconds
 }).then(() => {
     console.log('MongoDB connected');
 }).catch(err => {
