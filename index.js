b7ee62f0ec31b539c7dfd449cba7162217626e6d
Juantorreser <128902947+Juantorreser@users.noreply.github.com>
2024-08-01 10:15:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,17 @@ app.use((req, res, next) => {
 });
 
 //mongoose connection
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+    sslValidate: true
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+});
+
 
 app.get("/", (req, res) => {
     res.send(`Node.js version: ${process.version}`);
